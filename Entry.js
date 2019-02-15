d6f6e2b4b0ad4cf826103a14ca8c00973d58681f
JaffarShah <sjeffshah@gmail.com>
2019-02-15 20:08:27 +0000

    Entry level question

---
+++ Entry.js
@@ -0,0 +1,10 @@
+/*This is the code I had to correct to enter the website. I found it pretty easy.
+function multiply(a, b){
+  a * b
+}
+*/
+//Solution:
+
+function multiply(a, b){
+    return a * b;
+  }
